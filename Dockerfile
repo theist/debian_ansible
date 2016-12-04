0b8f3400ecb5717771bcc905f33eb7e0d2de4075
Carlos Peñas <carlos.penas@the-cocktail.com>
2016-12-04 19:35:53 +0100

    Reverted all systemd stuff
    Revert "moved all stuff out of /tmp"
    
    This reverts commit 449357c7dfd9c52214c0d853de22a948b638679a.
    
    Revert "updated doc"
    
    This reverts commit 9ecc4d6ea57bb3a90a21014cd53c47aca68951a0.
    
    Revert "install & run ansible in container"
    
    This reverts commit 5dd82c063cbaa50e88a251ce005731dc5888bfe2.

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,10 @@
 FROM debian
 MAINTAINER Carlos Peñas
 RUN apt-get -y update
-RUN apt-get install -y python-dev python-pip libffi-dev python-setuptools pypy-lib libyaml-dev libgnutls-openssl-dev libssl-dev systemd
+RUN apt-get install -y python-dev python-pip libffi-dev python-setuptools pypy-lib libyaml-dev libgnutls-openssl-dev libssl-dev
 RUN pip install cffi --upgrade
 RUN pip install cryptography --upgrade
 RUN pip install ansible
-RUN cd /lib/systemd/system/sysinit.target.wants/; ls | grep -v systemd-tmpfiles-setup | xargs rm -f $1 \
-rm -f /lib/systemd/system/multi-user.target.wants/*;\
-rm -f /etc/systemd/system/*.wants/*;\
-rm -f /lib/systemd/system/local-fs.target.wants/*; \
-rm -f /lib/systemd/system/sockets.target.wants/*udev*; \
-rm -f /lib/systemd/system/sockets.target.wants/*initctl*; \
-rm -f /lib/systemd/system/basic.target.wants/*;\
-rm -f /lib/systemd/system/anaconda.target.wants/*; \
-rm -f /lib/systemd/system/plymouth*; \
-rm -f /lib/systemd/system/systemd-update-utmp* \
-rm -f /lib/systemd/system/tmp.mount \
-rm -f /lib/systemd/system/systemd-tmpfiles-clean.timer;
-RUN systemctl set-default multi-user.target
-ENV init /lib/systemd/systemd
-VOLUME [ "/sys/fs/cgroup" ]
 
 COPY ansible_and_bash.sh /ansible_and_bash.sh
 RUN chmod a+x /ansible_and_bash.sh
--- ansible_and_bash.sh
+++ ansible_and_bash.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-/lib/systemd/systemd --system &
-
-sleep 5
-
-if [ ! -d /ansible ]; then
+if [ ! -d /tmp/ansible ]; then
   echo "ansible directory not present please run container with -v \$PDW:/tmp/ansile"
   exit 1
 fi
 
-echo "[all]" > /ansible/test_inventory
-echo "127.0.0.1" >> /ansible/test_inventory
+echo "[all]" > /tmp/ansible/test_inventory
+echo "127.0.0.1" >> /tmp/ansible/test_inventory
 if [ "$TEST_GROUPS" ]; then
   echo "Additonal groups $TEST_GROUPS"
   for group in $TEST_GROUPS; do
-    echo "[$group]" >> /ansible/test_inventory
-    echo "127.0.0.1" >> /ansible/test_inventory
+    echo "[$group]" >> /tmp/ansible/test_inventory
+    echo "127.0.0.1" >> /tmp/ansible/test_inventory
   done
 else
   echo 'No additional groups. You can add groups via -e "TEST_GROUPS=<groups>"'
 fi
 
-cd /ansible
-ansible-playbook --inventory-file=/ansible/test_inventory --list-tasks site.yml
-ansible-playbook --inventory-file=/ansible/test_inventory -c local site.yml
-echo ansible-playbook --inventory-file=/ansible/test_inventory -c local site.yml
+cd /tmp/ansible
+ansible-playbook --inventory-file=/tmp/ansible/test_inventory --list-tasks site.yml
+ansible-playbook --inventory-file=/tmp/ansible/test_inventory -c local site.yml
+echo ansible-playbook --inventory-file=/tmp/ansible/test_inventory -c local site.yml
 
 /bin/bash
 
-rm /ansible/test_inventory
+rm /tmp/ansible/test_inventory
